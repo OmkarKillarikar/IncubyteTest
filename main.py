aa2cf68ecff6804e94f8cbe48f351e7377c17008
Omkar Killarikar <okmac@neuron.local>
2022-03-26 19:59:44 +0530

    custom delimiter scenario

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ def add(input_str):
             return 0
         else:
             has_delimiter = any(delimiter in input_str for delimiter in delimiters)
-            if delimiter_flag not in input_str and not has_delimiter:
+            has_custom_delimiter = delimiter_flag in input_str
+
+            if not has_custom_delimiter and not has_delimiter:
                 return int(input_str)
-            else:
+            elif has_custom_delimiter:
+                custom_delimiter = input_str[2]
+                input_str = input_str[3:]
+                numbers = input_str.split(custom_delimiter)
+                addition = 0
+                for n in numbers:
+                    addition += int(n)
+                return addition
+            elif has_delimiter and not has_custom_delimiter:
                 numbers = re.split(delimiters, input_str)
                 addition = 0
                 for n in numbers:
@@ -23,4 +33,4 @@ def add(input_str):
 
 
 if __name__ == '__main__':
-    print(add("1,4"))
+    print(add("//?\n1?2,1"))
--- tests.py
+++ tests.py
@@ -20,11 +20,16 @@ def test_comma():
     assert add("0,2,2,6,5,10") == 25
 
 
-def test_semicolon():
+def test_newline():
     assert add("3\n2") == 5
-    assert add("0\n2") == 2
+    assert add("0\n2\n7") == 9
 
 
-def test_comma_semicolon():
+def test_comma_newline():
     assert add("3\n2,3") == 8
-    assert add("0,2\n5") == 7
+    assert add("0,2\n5\n3,10") == 20
+
+
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
+    assert add("//?\n1?9?1") == 11
