35e38f8d06af2ee1a9844d3fbc7b146759130a0e
Omkar Killarikar <okmac@neuron.local>
2022-03-26 19:15:00 +0530

    single digit, comma

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import re
+
+delimiter_flag = "//"
+delimiters = ",|;"
+
+
 def add(input_str):
     if input_str is None:
         return None
     else:
         if len(input_str) == 0:
             return 0
+        else:
+            has_delimiter = any(delimiter in input_str for delimiter in delimiters)
+            if delimiter_flag not in input_str and not has_delimiter:
+                return int(input_str)
+            else:
+                numbers = re.split(delimiters, input_str)
+                addition = 0
+                for n in numbers:
+                    addition += int(n)
+                return addition
 
 
 if __name__ == '__main__':
-    print(add("1"))
+    print(add("1,4"))
--- tests.py
+++ tests.py
@@ -7,3 +7,13 @@ def test_none():
 
 def test_empty():
     assert add("") == 0
+
+
+def test_single():
+    assert add("1") == 1
+    assert add("345") == 345
+
+
+def test_commas():
+    assert add("3,2") == 5
+    assert add("0,2") == 2
